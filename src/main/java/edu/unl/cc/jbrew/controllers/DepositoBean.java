1d0bc57cd5253a15ca0d9aee5595d5fc84d865a0
Joseph Adrian <josephadrianaguilar@gmail.com>
2025-07-27 23:26:08 -0500

    DepositoBean.java

--- src/main/java/edu/unl/cc/jbrew/controllers/DepositoBean.java
+++ src/main/java/edu/unl/cc/jbrew/controllers/DepositoBean.java
@@ -1,3 +1,10 @@
+/**
+  Created by IntelliJ IDEA.
+  User: Joseph Aguilar
+  Date: 24/7/25
+  Time: 21:38
+*/
+
 package edu.unl.cc.jbrew.controllers;
 
 import edu.unl.cc.jbrew.domain.common.Deposito;
